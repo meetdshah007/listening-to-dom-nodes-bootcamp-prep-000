acd0d22b1609802756d28df7725976b3126f29f9
Meet Shah <meetdshah007@gmail.com>
2019-02-27 11:12:14 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,5 +7,9 @@ main.addEventListener('click', function(ev){
 
 const input = document.querySelector('input');
 input.addEventListener('keydown', function(e) {
-  console.log(e.key);
+  if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
 });
